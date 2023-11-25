e694ade2dea7336f2d52a96328171fd4980826f9
Paul Straw <paulstraw@paulstraw.com>
2023-11-24 23:21:07 -0800

    Add debug output and more controls to example 1

--- examples/pdbmp_01_basic/README.md
+++ examples/pdbmp_01_basic/README.md
@@ -3,3 +3,10 @@
 ```
 nimble all2 && open pdbmp_01_basic.pdx
 ```
+
+* Left/right: Change image
+* Up/down: Change image scale
+* B + D-pad: Scroll
+* A: Change dither mode
+* Undock crank: Sample all pixels every frame
+	* Only used to show sample duration
--- examples/pdbmp_01_basic/src/pdbmp_01_basic.nim
+++ examples/pdbmp_01_basic/src/pdbmp_01_basic.nim
@@ -1,10 +1,13 @@
+import strutils
+import math
+import std/importutils
+
 import playdate/api
+import playdate/bindings/types
 
 import ../../../src/pdbmp
 
 import ditherPatterns
-
-var img: LCDBitmap
 
 let bmps = @[
   PdBmp(filePath: "bmp/aseprite/indexed-4bpp-grayscale-24-24.bmp"),
@@ -45,30 +48,52 @@ let bmps = @[
 
 var bmpIndex = 0
 var bmpScale = 2
+var isBButtonDown = false
+var imgX = 0
+var imgY = 0
+var buttonState: tuple[current: PDButtons, pushed: PDButtons,
+    released: PDButtons]
+var ditherMode = 0
+
+var font: LCDFont
+var img: LCDBitmap
+var infoImg: LCDBitmap
+
+const scrollSpeed = 4
+const ditherNames = @[
+  "gb4Light",
+  "gb4Dark",
+  "gb5",
+  "bayer8x8",
+]
+
+proc strokeOffsets(radius: int): seq[tuple[x: int, y: int]] =
+  result = @[]
+  for x in -radius..radius:
+    for y in -radius..radius:
+      if x*x + y*y <= radius*radius:
+        result.add((x, y))
 
 proc loadBmp(bmpIndex: int, bmpScale: int): void =
   try:
-    let startMs = playdate.system.getCurrentTimeMilliseconds()
+    let startAt = playdate.system.getElapsedTime()
     var bmp = bmps[bmpIndex]
     bmp.load()
 
-    let loadedAtMs = playdate.system.getCurrentTimeMilliseconds()
-    playdate.system.logToConsole("Load in " & $(loadedAtMs - startMs))
-
+    let bmpLoadAt = playdate.system.getElapsedTime()
+    let bmpLoadDur = bmpLoadAt - startAt
 
     let bmpWidth = bmp.dibHeader.imageWidth
     let bmpHeight = bmp.dibHeader.imageHeight
-
-    playdate.system.logToConsole("wh" & $(bmpWidth) & "," & $(bmpHeight))
-    playdate.system.logToConsole("bpp" & $(bmp.dibHeader.bitsPerPixel))
+    let bpp = bmp.dibHeader.bitsPerPixel
 
     img = playdate.graphics.newBitmap(
       bmpWidth * bmpScale, bmpHeight * bmpScale,
       LCDSolidColor.kColorClear
     )
 
-    let imgInitAt = playdate.system.getCurrentTimeMilliseconds()
-    playdate.system.logToConsole("Img init in " & $(imgInitAt - loadedAtMs))
+    let imgInitAt = playdate.system.getElapsedTime()
+    let imgInitDur = imgInitAt - bmpLoadAt
 
     playdate.graphics.pushContext(img)
     for x in 0..<bmpWidth:
@@ -83,60 +108,204 @@ proc loadBmp(bmpIndex: int, bmpScale: int): void =
 
         playdate.graphics.fillRect(
           x * bmpScale, y * bmpScale, bmpScale, bmpScale,
-          bayer8x8[int(luma) div 4],
-          # gb4Light[int(luma) div 64],
-            # gb4Dark[int(luma) div 64],
-            # gb5[int(luma) div 51],
+          case ditherMode:
+          of 0:
+            gb4Light[int(luma) div 64]
+          of 1:
+            gb4Dark[int(luma) div 64]
+          of 2:
+            gb5[int(luma) div 51]
+          else:
+            bayer8x8[int(luma) div 4]
         )
     playdate.graphics.popContext()
 
-    let imgAtMs = playdate.system.getCurrentTimeMilliseconds()
-    playdate.system.logToConsole("Img rendered in " & $(imgAtMs - imgInitAt))
+    let imgDrawnAt = playdate.system.getElapsedTime()
+    let imgDrawDur = imgDrawnAt - imgInitAt
+
+    let sepInd = bmp.filePath.rfind("/")
+    let fileName = bmp.filePath[sepInd+1..^1]
+    let infoStrings = @[
+      "w,h,bpp: " & $(bmpWidth) & "," & $(bmpHeight) & "," & $(bpp),
+      "dither: " & $(ditherNames[ditherMode]),
+      "bmpLoad: " & $((bmpLoadDur * 1000).formatFloat(ffDecimal, 2)) & "ms",
+      "imgInit: " & $((imgInitDur * 1000).formatFloat(ffDecimal, 2)) & "ms",
+      "imgDraw: " & $((imgDrawDur * 1000).formatFloat(ffDecimal, 2)) & "ms",
+      fileName,
+    ]
+
+    let infoImgWidth = 400
+    let fontHeight = font.getFontHeight().int
+    let infoImgHeight = fontHeight * infoStrings.len + 4
+    let newInfoImg = playdate.graphics.newBitmap(
+      infoImgWidth,
+      infoImgHeight,
+      LCDSolidColor.kColorClear,
+    )
+
+    playdate.graphics.pushContext(newInfoImg)
+
+    for i in 0..<infoStrings.len:
+      let str = infoStrings[i]
+      let strWidth = font.getTextWidth(
+        str,
+        str.len,
+        PDStringEncoding.kUTF8Encoding,
+        0
+      )
+      playdate.graphics.drawText(
+        str,
+        str.len.uint,
+        PDStringEncoding.kUTF8Encoding,
+        infoImgWidth - strWidth,
+        i * fontHeight
+      )
+
+    playdate.graphics.popContext()
+
+    infoImg = playdate.graphics.newBitmap(
+      infoImgWidth,
+      infoImgHeight,
+      LCDSolidColor.kColorClear,
+    )
+    playdate.graphics.pushContext(infoImg)
+    let offsets = strokeOffsets(2)
+    playdate.graphics.setDrawMode(LCDBitmapDrawMode.kDrawModeFillWhite)
+    for offset in offsets:
+      newInfoImg.draw(offset.x, offset.y + 2,
+          LCDBitmapFlip.kBitmapUnflipped)
+
+    playdate.graphics.setDrawMode(LCDBitmapDrawMode.kDrawModeFillBlack)
+    newInfoImg.draw(0, 2, LCDBitmapFlip.kBitmapUnflipped)
+    playdate.graphics.popContext()
   except Exception as e:
     playdate.system.logToConsole("Error parsing BMP: " & e.msg)
 
 proc handleInit(): void =
   playdate.display.setRefreshRate(50)
+
+  font = try: playdate.graphics.newFont(
+      "fonts/nico/nico-clean-16.pft") except: nil
+  playdate.graphics.setFont(font)
+
+  if font == nil:
+    playdate.system.logToConsole("Error loading font")
+    return
+
   loadBmp(bmpIndex, bmpScale)
 
 proc update(): int {.raises: [].} =
-  let buttonState = playdate.system.getButtonsState()
+  buttonState = playdate.system.getButtonsState()
+
   var needsLoad = false
+  var needsPosReset = false
   let oldBmpIndex = bmpIndex
 
+  if buttonState.pushed.contains(PDButton.kButtonB):
+    isBButtonDown = true
+  if buttonState.released.contains(PDButton.kButtonB):
+    isBButtonDown = false
+
+  # Scroll while B held
+  if buttonState.current.contains(PDButton.kButtonUp):
+    if isBButtonDown: imgY += scrollSpeed
+  if buttonState.current.contains(PDButton.kButtonRight):
+    if isBButtonDown: imgX -= scrollSpeed
+  if buttonState.current.contains(PDButton.kButtonDown):
+    if isBButtonDown: imgY -= scrollSpeed
+  if buttonState.current.contains(PDButton.kButtonLeft):
+    if isBButtonDown: imgX += scrollSpeed
+
+  # Prev image
   if buttonState.pushed.contains(PDButton.kButtonLeft):
+    if isBButtonDown: return
     bmpIndex -= 1
     if bmpIndex < 0:
       bmpIndex = bmps.len - 1
     needsLoad = true
+    needsPosReset = true
 
+  # Next image
   if buttonState.pushed.contains(PDButton.kButtonRight):
+    if isBButtonDown: return
     bmpIndex += 1
     if bmpIndex >= bmps.len:
       bmpIndex = 0
     needsLoad = true
+    needsPosReset = true
 
+  # Increase scale
   if buttonState.pushed.contains(PDButton.kButtonUp):
+    if isBButtonDown: return
     bmpScale += 1
     if bmpScale > 4:
       bmpScale = 4
     needsLoad = true
+    needsPosReset = true
 
+  # Decrease scale
   if buttonState.pushed.contains(PDButton.kButtonDown):
+    if isBButtonDown: return
     bmpScale -= 1
     if bmpScale < 1:
       bmpScale = 1
     needsLoad = true
+    needsPosReset = true
 
+  # Change dither
+  if buttonState.pushed.contains(PDButton.kButtonA):
+    needsLoad = true
+    ditherMode += 1
+    if ditherMode >= ditherNames.len:
+      ditherMode = 0
+
+  # Change image if necessary
   if needsLoad:
     var oldBmp = bmps[oldBmpIndex]
     oldBmp.unload()
 
     loadBmp(bmpIndex, bmpScale)
 
+    if needsPosReset:
+      let bmp = bmps[bmpIndex]
+      imgX = 200 - bmp.dibHeader.imageWidth * bmpScale div 2
+      imgY = 120 - bmp.dibHeader.imageHeight * bmpScale div 2
+
   playdate.graphics.clear(LCDSolidColor.kColorWhite)
   if img != nil:
-    img.draw(0, 0, LCDBitmapFlip.kBitmapUnflipped)
+    img.draw(imgX, imgY, LCDBitmapFlip.kBitmapUnflipped)
+
+  if infoImg != nil:
+    infoImg.draw(
+      400 - 5 - infoImg.width,
+      240 - 5 - infoImg.height,
+      LCDBitmapFlip.kBitmapUnflipped
+    )
+
+  # When crank is undocked, sample all pixels every frame
+  if not playdate.system.isCrankDocked:
+    let currentBmp = bmps[bmpIndex]
+    let bmpWidth = currentBmp.dibHeader.imageWidth
+    let bmpHeight = currentBmp.dibHeader.imageHeight
+
+    let sampleStart = playdate.system.getElapsedTime()
+    for y in 0..<bmpHeight:
+      for x in 0..<bmpWidth:
+        # Just discard the sample, don't actually do anything with it
+        discard try: currentBmp.sample(x.uint32, y.uint32) except: (0, 0, 0, 0)
+
+    let sampleDur = playdate.system.getElapsedTime() - sampleStart
+    let sampleStr = "Sample: " & $((sampleDur * 1000).formatFloat(ffDecimal,
+        2)) & "ms"
+    let sampleStrWidth = font.getTextWidth(sampleStr, sampleStr.len,
+        PDStringEncoding.kUTF8Encoding, 0)
+    playdate.graphics.drawText(
+      sampleStr,
+      sampleStr.len.uint,
+      PDStringEncoding.kUTF8Encoding,
+      400 - 5 - sampleStrWidth,
+      5.int,
+    )
 
   playdate.system.drawFPS(0, 0)
 
